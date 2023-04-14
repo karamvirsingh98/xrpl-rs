dce73602b833069e84830872e3b0d7215042994d
Karamvir Singh <karamvir.singh98@gmail.com>
2023-04-14 16:51:23 -0700

    disable unimplemented requests

--- src/types/request/mod.rs
+++ src/types/request/mod.rs
@@ -3,34 +3,30 @@ use serde_json::json;
 #[derive(Debug)]
 pub enum XrplRequest {
     ServerInfoRequest,
-    AccountChannelsRequest {
-        account: String,
-    },
-    AccountCurrenciesRequest {
-        account: String,
-    },
-    AccountInfoRequest {
-        account: String,
-    },
-    AccountLinesRequest {
-        account: String,
-    },
-    AccountNFTsRequest {
-        account: String,
-    },
-    AccountObjectsRequest {
-        account: String,
-    },
-    AccountOffersRequest {
-        account: String,
-    },
-    AccountTxRequest {
-        account: String,
-    },
-    BookOffersRequest {
-        taker_gets: String,
-        taker_pays: String,
-    },
+    // AccountChannelsRequest { account: String },
+    // AccountCurrenciesRequest {
+    //     account: String,
+    // },
+    AccountInfoRequest { account: String },
+    // AccountLinesRequest {
+    //     account: String,
+    // },
+    // AccountNFTsRequest {
+    //     account: String,
+    // },
+    // AccountObjectsRequest {
+    //     account: String,
+    // },
+    // AccountOffersRequest {
+    //     account: String,
+    // },
+    // AccountTxRequest {
+    //     account: String,
+    // },
+    // BookOffersRequest {
+    //     taker_gets: String,
+    //     taker_pays: String,
+    // },
 }
 
 impl XrplRequest {
@@ -39,34 +35,34 @@ impl XrplRequest {
             XrplRequest::ServerInfoRequest => {
                 json!({ "id": id, "command": "server_info" }).to_string()
             }
-            XrplRequest::AccountChannelsRequest { account } => {
-                json!({ "id": id, "command": "account_channels", "account": account }).to_string()
-            }
-            XrplRequest::AccountCurrenciesRequest { account } => {
-                json!({ "id": id, "command": "account_currencies", "account": account }).to_string()
-            }
+            // XrplRequest::AccountChannelsRequest { account } => {
+            //     json!({ "id": id, "command": "account_channels", "account": account }).to_string()
+            // }
+            // XrplRequest::AccountCurrenciesRequest { account } => {
+            //     json!({ "id": id, "command": "account_currencies", "account": account }).to_string()
+            // }
             XrplRequest::AccountInfoRequest { account } => {
                 json!({ "id": id, "command": "account_info", "account": account }).to_string()
             }
-            XrplRequest::AccountLinesRequest { account } => {
-                json!({ "id": id, "command": "account_info", "account": account }).to_string()
-            }
-            XrplRequest::AccountNFTsRequest { account } => {
-                json!({ "id": id, "command": "account_info", "account": account }).to_string()
-            }
-            XrplRequest::AccountObjectsRequest { account } => {
-                json!({ "id": id, "command": "account_info", "account": account }).to_string()
-            }
-            XrplRequest::AccountOffersRequest { account } => {
-                json!({ "id": id, "command": "account_offers", "account": account }).to_string()
-            }
-            XrplRequest::AccountTxRequest { account } => {
-                json!({ "id": id, "command": "account_tx", "account": account }).to_string()
-            }
-            XrplRequest::BookOffersRequest {
-                taker_gets,
-                taker_pays,
-            } => json!({ "id": id, "command": "book_offers", "taker_gets": taker_gets, "taker_pays": taker_pays }).to_string(),
+            // XrplRequest::AccountLinesRequest { account } => {
+            //     json!({ "id": id, "command": "account_info", "account": account }).to_string()
+            // }
+            // XrplRequest::AccountNFTsRequest { account } => {
+            //     json!({ "id": id, "command": "account_info", "account": account }).to_string()
+            // }
+            // XrplRequest::AccountObjectsRequest { account } => {
+            //     json!({ "id": id, "command": "account_info", "account": account }).to_string()
+            // }
+            // XrplRequest::AccountOffersRequest { account } => {
+            //     json!({ "id": id, "command": "account_offers", "account": account }).to_string()
+            // }
+            // XrplRequest::AccountTxRequest { account } => {
+            //     json!({ "id": id, "command": "account_tx", "account": account }).to_string()
+            // }
+            // XrplRequest::BookOffersRequest {
+            //     taker_gets,
+            //     taker_pays,
+            // } => json!({ "id": id, "command": "book_offers", "taker_gets": taker_gets, "taker_pays": taker_pays }).to_string(),
         }
     }
 }
